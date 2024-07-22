3b2f60df18b937828f5affab33ab4fe4b430802a
Balaji Koneti <balajishiva2580@gmail.com>
2024-07-22 11:30:19 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const querystring = require('querystring');
 
 const server = http.createServer((req, res) => {
     const { method, url } = req;
+    console.log(`${method} request for ${url}`);
 
+    // Serve the index.html file
     if (url === '/' && method === 'GET') {
         fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
             if (err) {
@@ -16,6 +18,8 @@ const server = http.createServer((req, res) => {
                 res.end(data);
             }
         });
+
+    // Serve the styles.css file
     } else if (url === '/styles.css' && method === 'GET') {
         fs.readFile(path.join(__dirname, 'styles.css'), (err, data) => {
             if (err) {
@@ -26,6 +30,8 @@ const server = http.createServer((req, res) => {
                 res.end(data);
             }
         });
+
+    // Serve the script.js file
     } else if (url === '/script.js' && method === 'GET') {
         fs.readFile(path.join(__dirname, 'script.js'), (err, data) => {
             if (err) {
@@ -36,6 +42,20 @@ const server = http.createServer((req, res) => {
                 res.end(data);
             }
         });
+
+    // Serve the codes.html file
+    } else if (url === '/codes.html' && method === 'GET') {
+        fs.readFile(path.join(__dirname, 'codes.html'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+
+    // Handle POST requests for setting cookies
     } else if (url === '/set-cookies' && method === 'POST') {
         let body = '';
 
@@ -55,6 +75,26 @@ const server = http.createServer((req, res) => {
                 res.end('Name and Email are required');
             }
         });
+
+    // Serve other static files based on URL
+    } else if (url.startsWith('/')) {
+        const filePath = path.join(__dirname, url === '/' ? 'index.html' : url);
+        const extname = path.extname(filePath);
+        const contentType = extname === '.css' ? 'text/css' :
+                            extname === '.js' ? 'application/javascript' :
+                            extname === '.html' ? 'text/html' : 'text/plain';
+
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': contentType });
+                res.end(data);
+            }
+        });
+
+    // Handle 404 errors for unknown routes
     } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Page Not Found');
